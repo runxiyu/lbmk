afc80b89ec4f3bd5a2a957a6781dcdd5cc206c9e
Leah Rowe <leah@libreboot.org>
2023-01-17 12:48:14 +0000

    util/nvmutil: update copyright years

--- util/nvmutil/nvmutil.c
+++ util/nvmutil/nvmutil.c
@@ -1,5 +1,5 @@
 /* 
- * Copyright (C) 2022 Leah Rowe <info@minifree.org>
+ * Copyright (C) 2022, 2023 Leah Rowe <info@minifree.org>
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the
