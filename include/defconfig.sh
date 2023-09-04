2cbc7eea9533c6da7a4b1cbc3408066aaef85f5a
Leah Rowe <leah@libreboot.org>
2023-09-04 16:16:53 +0100

    update/blobs/*: unify checking of defconfig files
    Signed-off-by: Leah Rowe <leah@libreboot.org>

---
+++ include/defconfig.sh
@@ -0,0 +1,11 @@
+# Copyright (c) 2023 Leah Rowe <leah@libreboot.org>
+# SPDX-License-Identifier: MIT
+
+check_defconfig()
+{
+	no_config="printf \"No target defconfig in %s\\n\" ${1} 1>&2; return 1"
+	for x in "${1}"/config/*; do
+		[ -f "${x}" ] && no_config=""
+	done
+	eval "${no_config}"
+}
