65a3269835f94d2b4090ae702fd6d0911c7ca76c
Leah Rowe <leah@libreboot.org>
2023-09-27 15:26:17 +0100

    include/blobutil: simplify setting empty strings
    use a for loop and eval to set them.
    
    Signed-off-by: Leah Rowe <leah@libreboot.org>

--- include/blobutil.sh
+++ include/blobutil.sh
@@ -9,32 +9,17 @@ _b=""
 blobdir="blobs"
 appdir="${blobdir}/app"
 
-ec_url=""
-ec_url_bkup=""
-ec_hash=""
-dl_hash=""
-dl_url=""
-dl_url_bkup=""
-dl_path=""
-e6400_vga_dl_hash=""
-e6400_vga_dl_url=""
-e6400_vga_dl_url_bkup=""
-e6400_vga_offset=""
-e6400_vga_romname=""
-sch5545ec_dl_url=""
-sch5545ec_dl_url_bkup=""
-sch5545ec_dl_hash=""
+for x in ec_url ec_url_bkup ec_hash dl_hash dl_url dl_url_bkup dl_path \
+    e6400_vga_dl_hash e6400_vga_dl_url e6400_vga_dl_url_bkup e6400_vga_offset \
+    e6400_vga_romname sch5545ec_dl_url sch5545ec_dl_url_bkup \
+    sch5545ec_dl_hash; do
+	eval "${x}=\"\""
+done
 
-sname=""
-archive=""
-_filetype=""
-rom=""
-board=""
-modifygbe=""
-new_mac=""
-release=""
-releasearchive=""
-vendor_rom=""
+for x in sname archive _filetype rom board modifygbe new_mac release \
+    releasearchive vendor_rom; do
+	eval "${x}=\"\""
+done
 
 cbdir="coreboot/default"
 cbcfgsdir="config/coreboot"
@@ -51,22 +36,12 @@ kbc1126_ec_dump="$(pwd)/${cbdir}/util/kbc1126/kbc1126_ec_dump"
 pfs_extract="$(pwd)/biosutilities/Dell_PFS_Extract.py"
 uefiextract="$(pwd)/uefitool/uefiextract"
 
-_me_destination=""
-_gbe_destination=""
-_ifd_destination=""
-
-CONFIG_BOARD_DELL_E6400=""
-CONFIG_HAVE_MRC=""
-CONFIG_HAVE_ME_BIN=""
-CONFIG_ME_BIN_PATH=""
-CONFIG_KBC1126_FIRMWARE=""
-CONFIG_KBC1126_FW1=""
-CONFIG_KBC1126_FW1_OFFSET=""
-CONFIG_KBC1126_FW2=""
-CONFIG_KBC1126_FW2_OFFSET=""
-CONFIG_VGA_BIOS_FILE=""
-CONFIG_VGA_BIOS_ID=""
-CONFIG_GBE_BIN_PATH=""
-CONFIG_INCLUDE_SMSC_SCH5545_EC_FW=""
-CONFIG_SMSC_SCH5545_EC_FW_FILE=""
-CONFIG_IFD_BIN_PATH=""
+for x in _me_destination _gbe_destination _ifd_destination \
+    CONFIG_BOARD_DELL_E6400 CONFIG_HAVE_MRC CONFIG_HAVE_ME_BIN \
+    CONFIG_ME_BIN_PATH CONFIG_KBC1126_FIRMWARE CONFIG_KBC1126_FW1 \
+    CONFIG_KBC1126_FW1_OFFSET CONFIG_KBC1126_FW2 CONFIG_KBC1126_FW2_OFFSET \
+    CONFIG_VGA_BIOS_FILE CONFIG_VGA_BIOS_ID CONFIG_GBE_BIN_PATH \
+    CONFIG_INCLUDE_SMSC_SCH5545_EC_FW CONFIG_SMSC_SCH5545_EC_FW_FILE \
+    CONFIG_IFD_BIN_PATH; do
+	eval "${x}=\"\""
+done
