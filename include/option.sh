5e4009b539b0c1bbb14eea8f8cd892d3933c3355
Leah Rowe <leah@libreboot.org>
2024-05-06 22:54:55 +0100

    merge include/err.sh with include/option.sh
    Signed-off-by: Leah Rowe <leah@libreboot.org>

--- include/option.sh
+++ include/option.sh
@@ -3,6 +3,10 @@
 # SPDX-FileCopyrightText: 2022 Ferass El Hafidi <vitali64pmemail@protonmail.com>
 # SPDX-FileCopyrightText: 2020-2024 Leah Rowe <leah@libreboot.org>
 
+export LC_COLLATE=C
+export LC_ALL=C
+
+tmpdir_was_set="y"
 vendir="vendorfiles"
 appdir="${vendir}/app"
 cbdir="src/coreboot/default"
@@ -13,22 +17,64 @@ grubcfgsdir="config/grub"
 layoutdir="/boot/grub/layouts"
 . "${grubcfgsdir}/modules.list"
 tmpgit="${PWD}/tmp/gitclone"
+err="err_"
 
+err_()
+{
+	printf "ERROR %s: %s\n" "${0}" "${1}" 1>&2
+	exit 1
+}
+
+setvars()
+{
+	_setvars=""
+	[ $# -lt 2 ] && $err "setvars: too few arguments"
+	val="${1}" && shift 1
+	for var in $@; do
+		_setvars="${var}=\"${val}\"; ${_setvars}"
+	done
+	printf "%s\n" "${_setvars% }"
+}
 eval "$(setvars "" CONFIG_BOARD_DELL_E6400 CONFIG_HAVE_MRC CONFIG_HAVE_ME_BIN \
-    CONFIG_ME_BIN_PATH CONFIG_KBC1126_FIRMWARE CONFIG_KBC1126_FW1 \
+    CONFIG_ME_BIN_PATH CONFIG_KBC1126_FIRMWARE CONFIG_KBC1126_FW1 _nogit \
     CONFIG_KBC1126_FW1_OFFSET CONFIG_KBC1126_FW2 CONFIG_KBC1126_FW2_OFFSET \
-    CONFIG_VGA_BIOS_FILE CONFIG_VGA_BIOS_ID CONFIG_GBE_BIN_PATH \
+    CONFIG_VGA_BIOS_FILE CONFIG_VGA_BIOS_ID CONFIG_GBE_BIN_PATH tmpdir \
     CONFIG_INCLUDE_SMSC_SCH5545_EC_FW CONFIG_SMSC_SCH5545_EC_FW_FILE \
-    CONFIG_IFD_BIN_PATH CONFIG_MRC_FILE _dest board boarddir \
-    CONFIG_HAVE_REFCODE_BLOB CONFIG_REFCODE_BLOB_FILE)"
+    CONFIG_IFD_BIN_PATH CONFIG_MRC_FILE _dest board boarddir lbmk_release \
+    CONFIG_HAVE_REFCODE_BLOB CONFIG_REFCODE_BLOB_FILE lbmk_status threads \
+    version versiondate projectname)"
 
-threads=
+# if "y": a coreboot target won't be built if target.cfg says release="n"
+# (this is used to exclude certain build targets from releases)
+set | grep LBMK_RELEASE 1>/dev/null 2>/dev/null || lbmk_release="n" || :
+[ -z "$lbmk_release" ] && lbmk_release="$LBMK_RELEASE"
+[ "$lbmk_release" = "n" ] || [ "$lbmk_release" = "y" ] || lbmk_release="n"
+export LBMK_RELEASE="$lbmk_release"
+
+# if set to n via export, status checks will be skipped during
+# all builds, and all targets will be built regardless of status.
+# this replicates the old behaviour of lbmk, prior to the checks.
+set | grep LBMK_STATUS 1>/dev/null 2>/dev/null && lbmk_status="$LBMK_STATUS"
+[ "$lbmk_status" = "y" ] || [ "$lbmk_status" = "n" ] || lbmk_status="y"
+export LBMK_STATUS="$lbmk_status"
+
+set | grep TMPDIR 1>/dev/null 2>/dev/null || tmpdir_was_set="n"
+if [ "${tmpdir_was_set}" = "y" ]; then
+	[ "${TMPDIR%_*}" = "/tmp/lbmk" ] || tmpdir_was_set="n"
+fi
+if [ "${tmpdir_was_set}" = "n" ]; then
+	export TMPDIR="/tmp"
+	tmpdir="$(mktemp -d -t lbmk_XXXXXXXX)"
+	export TMPDIR="${tmpdir}"
+else
+	export TMPDIR="${TMPDIR}"
+	tmpdir="${TMPDIR}"
+fi
+
 set | grep LBMK_THREADS 1>/dev/null 2>/dev/null && threads="$LBMK_THREADS"
 [ -z "$threads" ] && threads=1
-
 expr "X$threads" : "X-\{0,1\}[0123456789][0123456789]*$" \
     1>/dev/null 2>/dev/null || threads=1 # user specified a non-integer
-
 export LBMK_THREADS="$threads"
 
 items()
@@ -91,4 +137,45 @@ remkdir()
 {
 	rm -Rf "${1}" || $err "remkdir: !rm -Rf \"${1}\""
 	mkdir -p "${1}" || $err "remkdir: !mkdir -p \"${1}\""
+}
+
+x_() {
+	[ $# -lt 1 ] || ${@} || $err "Unhandled non-zero exit: $@"; return 0
+}
+
+check_git()
+{
+	which git 1>/dev/null 2>/dev/null || \
+	    git_err "git not installed. please install git-scm."
+	git config --global user.name 1>/dev/null 2>/dev/null || \
+	    git_err "git config --global user.name \"John Doe\""
+	git config --global user.email 1>/dev/null 2>/dev/null || \
+	    git_err "git config --global user.email \"john.doe@example.com\""
+}
+
+git_err()
+{
+	printf "You need to set git name/email, like so:\n%s\n\n" "$1" 1>&2
+	$err "Git name/email not configured"
+}
+
+check_project()
+{
+	read -r projectname < projectname || :
+
+	[ ! -f version ] || read -r version < version || :
+	version_="${version}"
+	[ ! -e ".git" ] || version="$(git describe --tags HEAD 2>&1)" || \
+	    version="git-$(git rev-parse HEAD 2>&1)" || version="${version_}"
+
+	[ ! -f versiondate ] || read -r versiondate < versiondate || :
+	versiondate_="${versiondate}"
+	[ ! -e ".git" ] || versiondate="$(git show --no-patch --no-notes \
+	    --pretty='%ct' HEAD)" || versiondate="${versiondate_}"
+
+	for p in projectname version versiondate; do
+		eval "[ -n \"\$$p\" ] || $err \"$p unset\""
+		eval "x_ printf \"%s\\n\" \"\$$p\" > $p"
+	done
+	export LOCALVERSION="-${projectname}-${version%%-*}"
 }
